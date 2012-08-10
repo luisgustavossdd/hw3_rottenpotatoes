5cf021902f6a8c03e1b45712c2713bd8736dbea5
saasbook <saasbook@saasbook.(none)>
2012-08-10 15:11:22 -0700

    HW3 PART 1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  regexp = Regexp.new ".*#{e1}.*#{e2}"
+  page.content =~ regexp
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +26,8 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split.each do |rating|
+    check = uncheck ? "uncheck" : "check"
+    When %Q{I #{check} #{rating}}
+  end
 end
