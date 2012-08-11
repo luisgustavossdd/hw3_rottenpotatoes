f9f0de3082188bc1391e30c9a43abfc833f3054c
saasbook <saasbook@saasbook.(none)>
2012-08-11 12:45:56 -0700

    HW3 Part 3

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -15,7 +15,7 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
   regexp = Regexp.new ".*#{e1}.*#{e2}"
-  page.content =~ regexp
+  page.body =~ regexp
 end
 
 # Make it easier to express checking or unchecking several boxes at once
